74ddb274b3d98c95b0a6fef5a48b596d2f2cbf19
BiehlA <36610944+biehla@users.noreply.github.com>
2023-03-28 21:23:53 -0400

    Banu's branch

--- src/Game.java
+++ src/Game.java
@@ -117,6 +117,17 @@ public class Game {
 
         return false;
     }
+    public boolean isBoardFull() {
+        int noneCount = 0;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i] == Marker.none) {
+                noneCount++;
+            }
+        }
+        return noneCount == 0;
+    }
+
+
 
 //    public static void main(String[] args) {
 //        Game game = new Game(3);
