5b34594fd75fc3384ae675ec2484884a01a7563c
BiehlA <36610944+biehla@users.noreply.github.com>
2023-03-31 23:54:45 -0400

    idfk

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,40 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
-        String playerName = scanner.nextLine();
+        System.out.println("Enter name: ");
+        String name = scanner.nextLine();
+        System.out.println("Choose your marker(x or o): ");
 
-        System.out.print("Choose your symbol (x or o): ");
-        Game.Marker playerMarker = Game.Marker.valueOf(scanner.nextLine());
+        Game.Marker marker = Game.Marker.valueOf(scanner.nextLine());
 
-        System.out.print("Choose difficulty (1 for the Easy Ai, 2 for for the Intelligent): ");
-        int difficulty = scanner.nextInt();
+        System.out.println("Choose who your opponent(1 for easy AI, 2 for intelligent AI, 3 for Human V Human): ");
+        int userChoose = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.print("Choose who goes first (1 for Player, 2 for Ai: ");
+
+        Game game = new Game(3);
+        Player player = new Player(marker, name);
+        Player opponent = null;
+        if (userChoose == 1) {
+            opponent = new EasyAiPlayer(marker== Game.Marker.x ? Game.Marker.o : Game.Marker.x);
+        } else if (userChoose == 2) {
+            opponent = new HardAiPlayer(marker == Game.Marker.x ? Game.Marker.o : Game.Marker.x);
+        } else if (userChoose == 3) {
+            System.out.println("Enter the name of the Player 2: ");
+            String secondPlayerName = scanner.nextLine();
+            System.out.println("Choose the symbol for the other player (x or o): ");
+            Game.Marker secondPlayerMarker = Game.Marker.valueOf(scanner.nextLine());
+            opponent = new Player(secondPlayerMarker, secondPlayerName);
+        } else {
+            System.out.println("Invalid");
+        }
+        System.out.println("Who goes first (1 for " + name + ", 2 for " + opponent.getName() + "): ");
         int turnChoose = scanner.nextInt();
         scanner.nextLine();
 
+<<<<<<< HEAD
         Game game = new Game(3);
         Player player = new Player(playerMarker, playerName);
         Player Ai;
@@ -44,33 +61,71 @@ public class Main {
 
         while (game.getWinner() == null && !game.isBoardFull()) {
             System.out.println(game);
+=======
+        Player currTurn = turnChoose == 1 ? player : opponent;
+        System.out.println();
+
+        while (game.getWinner()== null && !game.isBoardFull()) {
+>>>>>>> banu
             if (currTurn == player) {
-                System.out.println("Your turn, " + playerName + "!");
-                System.out.print("Enter row: ");
+                // player's turn
+                System.out.println("Your turn, " + name);
+                System.out.print("Enter empty horizantal row (0-2): ");
                 int row = scanner.nextInt();
-                System.out.print("Enter column: ");
-                int col = scanner.nextInt();
-                if (game.placeMarker(playerMarker, row, col)) {
-                    currTurn = Ai;
+                System.out.println("Enter vertical column (0-2): ");
+                int column = scanner.nextInt();
+                if (game.placeMarker(marker, row, column)) {
+                    if (game.checkBoard()) {
+                        break;
+                    }
+                    currTurn = opponent;
                 } else {
                     System.out.println("Invalid move, try again.");
                 }
-            } else {
-                System.out.println("Ai Turne");
-//                Ai.makeMove(game); error
+                // easy AI turn
+            } else if (currTurn instanceof EasyAiPlayer) {
+
+                System.out.println("Random Ai  turn");
+                ((EasyAiPlayer) currTurn).makeMove(game);
+                if (game.checkBoard()) {
+                    break;
+                }
                 currTurn = player;
+                // intelligent AI's turn
+            } else if (currTurn instanceof HardAiPlayer) {
+                System.out.println("Intelligent AI's turn");
+//                ((HardAiPlayer) currTurn).makeMove(game);
+                if (game.checkBoard()) {
+                    break;
+                }
+                currTurn = player;
+            } else {
+                // other human player's turn
+                System.out.println("Your turn, " + opponent.getName());
+                System.out.println("Enter empty horizantal row (0-2): ");
+                int row = scanner.nextInt();
+                System.out.println("Enter vertical column (0-2): ");
+                int column = scanner.nextInt();
+                if (game.placeMarker(opponent.getMarker(), row, column)) {
+                    if (game.checkBoard()) {
+                        break;
+                    }
+                    currTurn = player;
+                } else {
+                    System.out.println("Move invalid");
+                }
             }
             System.out.println(game);
         }
-
         if (game.getWinner() == null) {
-            System.out.println("Game ends in tie");
+            System.out.println("Game tied");
         } else {
-            String winName = game.getWinner() == playerMarker ? playerName : currTurn.getName();
-            System.out.println(winName + " wins");
+            String wName = game.getWinner()==marker?name:opponent.getName();
+
+            System.out.println(wName + " wins XD");
+            System.out.println();
+            System.out.println(game);
         }
 
-        System.out.println();
-        System.out.println(game);
     }
 }
